63c49c31e8af326d80b24e55166e36acc4e5f56a
lj1981 <97998057+lj1981@users.noreply.github.com>
2022-05-14 13:14:59 -0300

    Initial commit

---
+++ config/routes.js
@@ -0,0 +1,41 @@
+const express =require("express") 
+const routes = expres.routes()
+
+
+let db = [
+    {"1" : { nome: "cliente 1" , idade: "20" } }, 
+    {"2" : { nome: "cliente 2" , idade: "22" } },
+    {"3" : { nome: "cliente 3" , idade: "23" } }
+]
+
+// Buscar Dados 
+routes.get("/",(req,res) => {
+     return res.json(db)
+})
+
+// Inserir Dados
+routes.post("/add",(req,res) => {
+    const body = req.body
+    if(!body)
+        return res,status(400).end() 
+
+        db.push(body)
+         return res.json(body)
+})
+
+routes.delete("/:id",(req,res) => {
+    const id =req.params.id
+
+let.newDB = db.filter(item => {
+   if (!item[id])
+    return item
+
+})
+      db = newdb
+      return res.send(nemdb)
+})
+     
+
+
+module.exports = routes
+
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express') 
+const morgan = require ("morgan")    
+const cors = require ("cors")
+const bodyparser = require ("body-parser")
+const routes = require("./config/routes")
+
+const app = express()
+
+app.use(morgan("dev"))
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+app.use(routers)
+
+
+
+app.listen(30000, () => { 
+    console.log("express started at index.js")
+})
